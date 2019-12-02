c4dacb990c663a3894e2d90c353f82cae767e348
Markus Göckeritz <markus@goeckeritz.ch>
2019-12-02 16:35:42 +0100

    moved outfile handlers2

--- remove_empty_lines.py
+++ remove_empty_lines.py
@@ -8,10 +8,12 @@ def remove_file(filename):
 
 files = sys.argv[1:]
 references = []
+outfile_handlers = []
 
 for file in files:
-    outfile = file + ".out"
-    remove_file(outfile)
+    outfile = file + ".out.txt"
+    outfile_handlers.append(open(outfile, 'w'))
+
     handler = open(file, 'r')
     references.append(handler.readlines())
 
@@ -25,10 +27,14 @@ print(" %s" % '\n '.join(files))
 print("Max line length detected: %s" % max_length)
 
 for i in range(max_length):
+    if i % 1000 == 0:
+        print("%d percent complete" % (i/max_length*100), end="\r")
+
     min_l = min([0 if x[i] == '\n' else len(x[i]) for x in references])
     has_empty_lines = min_l == 0
 
     if not has_empty_lines:
-        for j in range(num_files):
-            handler = open(files[j]+'.out', 'a')
-            handler.write(references[j][i])
+        for j in range(len(outfile_handlers)):
+            outfile_handlers[j].write(references[j][i])
+    else:
+        print("Skipping line %d" % i)
